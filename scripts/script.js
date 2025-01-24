ce3bca6b8b8294c472d82c9f033da3ec863afdb7
Oscar Falk <oscar.falk@gmail.com>
2025-01-24 13:17:51 +0100

    added getMatches function

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,25 @@
 //GLOBAL NON DOM-OBJECT DECLARATIONS
+const imageDisplay=document.getElementById('image-display');
 
 //GLOBAL DOM-OBJECT DECLARATIONS
 
 //GLOBAL EVENT LISTENERS
 
 //GLOBAL FUNCTIONS
+
+async function getMatches(){
+    try{
+        let response=await fetch('https://sumo-api.com/api/rikishis?shikonaEn=Terunofuji');
+
+        if(!response.ok)
+            throw new Error(`${response.status}`);
+        console.log(response);
+        const data=await response.json();
+        console.log(data.results);
+    }
+    catch(error){
+        console.error("Error: " , error)
+    }
+};
+
+getMatches();
